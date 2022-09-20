48bec8184d4913a5f85240be4a968bfcbd9a7df3
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-20 19:37:30 +0300

    Bug Fixed0002

--- pages/profile/realty/index.tsx
+++ pages/profile/realty/index.tsx
@@ -9,6 +9,13 @@ import { API_URL } from '../../api/config';
 
 const RealtySetting: NextPage = () => {
     const [realty, setRealty] = useState<any[]>([]);
+    const deleteQ = async (id:any) => {
+        const res = await fetch(API_URL + `/realty/` + id, {
+            method: 'DELETE',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }
+        });
+        window.location.reload();
+    }
     useEffect(() => {
         axios.get(API_URL + '/realty')
             .then((response: AxiosResponse) => {
@@ -21,7 +28,8 @@ const RealtySetting: NextPage = () => {
         <ProfileContainers>
          {realty.map(item=>
         <div key={item.id}>
-           {item.email}
+            <p>{item.email}</p>
+            <button onClick={() => deleteQ(item.id)}>Удалить</button>
         </div>
         )}
         </ProfileContainers>
