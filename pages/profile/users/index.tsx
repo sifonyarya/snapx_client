85a7d9c1623321e150e3a60a3de5c791b63d5d09
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-20 23:25:40 +0300

    Bug Fixed0005

--- pages/profile/users/index.tsx
+++ pages/profile/users/index.tsx
@@ -1,6 +1,9 @@
+import axios, { AxiosResponse } from 'axios';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import ProfileContainers from '../../../containers/ProfileContainers';
+import { API_URL } from '../../api/config';
 
 
 
@@ -9,11 +12,22 @@ import ProfileContainers from '../../../containers/ProfileContainers';
 
 
 const UsersSettings: NextPage = () => {
+    const [users, setUsers] = useState<any[]>([]);
+    useEffect(() => {
+        axios.get(API_URL + '/users')
+            .then((response: AxiosResponse) => {
+                setUsers(response.data);
+            });
+    }, []);
     return (
         <>
         <Head><title>Все пользователи</title></Head>
         <ProfileContainers>
-        Все пользователи
+          {users.map(item=>
+          <div key={item.id}>
+              <p>{item.email}</p>
+          </div>  
+          )}
         </ProfileContainers>
         </>
     )
