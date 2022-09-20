e8f4871d7cb5e0fee7d439268175033dddacaae5
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-20 15:15:05 +0300

    Bug Fixed010

--- pages/profile/realty/index.tsx
+++ pages/profile/realty/index.tsx
@@ -1,15 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import ProfileContainers from '../../../containers/ProfileContainers';
+import { API_URL } from '../../api/config';
 
 
 
 const RealtySetting: NextPage = () => {
+    const [realty, setRealty] = useState<any[]>([]);
+    useEffect(() => {
+        axios.get(API_URL + '/realty')
+            .then((response: AxiosResponse) => {
+                setRealty(response.data);
+            });
+    }, []);
     return (
         <>
         <Head><title>Настройка недвижимости</title></Head>
         <ProfileContainers>
-        Настройка недвижимости
+         {realty.map(item=>
+        <div key={item.id}>
+           {item.email}
+        </div>
+        )}
         </ProfileContainers>
         </>
     )
