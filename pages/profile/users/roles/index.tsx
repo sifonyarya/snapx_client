915b69984ba56a4270875eb54403087aada4b322
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-25 14:27:35 +0300

    Bug Fixed0021

--- pages/profile/users/roles/index.tsx
+++ pages/profile/users/roles/index.tsx
@@ -1,17 +1,31 @@
+import axios, { AxiosResponse } from "axios";
 import { NextPage} from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import ProfileContainers from "../../../../containers/ProfileContainers";
+import { API_URL } from "../../../api/config";
 
 
 
 
 
 const RolesSettings: NextPage = () => {
+    const [roles, setRoles] = useState<any[]>([]);
+    useEffect(() => {
+        axios.get(API_URL + '/roles')
+            .then((response: AxiosResponse) => {
+                setRoles(response.data);
+            });
+    }, []);
     return (
         <>
         <Head><title>Роли пользователей</title></Head>
         <ProfileContainers>
-            Роли пользователей
+            {roles.map(item=>
+            <div key={item.id}>
+                <p>{item.name}</p>
+            </div>
+            )}
         </ProfileContainers>
         </>
     )
