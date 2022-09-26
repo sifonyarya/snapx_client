a5cc2d3f41c8c8296e62e53557aef439cef67893
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-26 04:10:06 +0300

    Bug Fixed0061

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import tw from 'twin.macro';
 import Head from "next/head";
 import MainComponents from "../containers/MainComponents";
 import Input from "../components/Input/Input";
@@ -16,38 +17,37 @@ const LoginPage: NextPage = () => {
     const router = useRouter();
     useEffect(() => {
         setTimeout(() => {
-          if (store.isAuth == true) {
-            router.push('/profile/profile');
-          }
-          }, 50);
-      }),
+            if (store.isAuth == true) {
+                router.push('/profile/profile');
+            }
+        }, 50);
+    }),
         [];
     return (
         <>
-        <Head><title>Авторизации</title></Head>
-        <MainComponents>
-            <br />
-            <br />
-            <br />
-            <br />
-            Вход на страницу
-            <Input
-            id="user_email"
-            type="email"
-            placeholder="Логин(Email)"
-            value={user_email}
-            onChange={(e:any) => setEmail(e.target.value)}
-            />
-            <Input
-            id="user_password"
-            type="password"
-            placeholder="Пароль"
-            value={user_password}
-            onChange={(e:any) => setPassword(e.target.value)}
-            />
-            <button onClick={() => store.login(user_email, user_password)}>Войти</button>
-        </MainComponents>
+            <Head><title>Авторизации</title></Head>
+            <MainComponents>
+                <LoginBlock>
+                    <Input
+                        id="user_email"
+                        type="email"
+                        placeholder="Логин(Email)"
+                        value={user_email}
+                        onChange={(e: any) => setEmail(e.target.value)}
+                    />
+                    <Input
+                        id="user_password"
+                        type="password"
+                        placeholder="Пароль"
+                        value={user_password}
+                        onChange={(e: any) => setPassword(e.target.value)}
+                    />
+                    <button onClick={() => store.login(user_email, user_password)}>Войти</button>
+                </LoginBlock>
+            </MainComponents>
         </>
     )
 }
 export default LoginPage;
+
+const LoginBlock = tw.div`w-[30%] block mx-auto`
