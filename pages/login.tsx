12eff279481b81fc57a89720bb0c955ca49d5f0f
sifonyarya <81504017+sifonyarya@users.noreply.github.com>
2022-09-26 11:50:00 +0300

    Bug Fixed0066

--- pages/login.tsx
+++ pages/login.tsx
@@ -25,30 +25,35 @@ const LoginPage: NextPage = () => {
         [];
     return (
         <>
-            <Head><title>Авторизации</title></Head>
+            <Head><title>Авторизация пользователя</title></Head>
             <MainComponents>
-                <LoginBlock>
-                    <Input
-                        id="user_email"
-                        type="email"
-                        placeholder="Логин(Email)"
-                        value={user_email}
-                        onChange={(e: any) => setEmail(e.target.value)}
-                    />
-                    <Input
-                        id="user_password"
-                        type="password"
-                        placeholder="Пароль"
-                        value={user_password}
-                        onChange={(e: any) => setPassword(e.target.value)}
-                    />
-                    <Login onClick={() => store.login(user_email, user_password)}>Войти</Login>
-                </LoginBlock>
+                <LoginContainer>
+                    <Title>Авторизация пользователя</Title>
+                    <LoginBlock>
+                        <Input
+                            id="user_email"
+                            type="email"
+                            placeholder="Логин(Email)"
+                            value={user_email}
+                            onChange={(e: any) => setEmail(e.target.value)}
+                        />
+                        <Input
+                            id="user_password"
+                            type="password"
+                            placeholder="Пароль"
+                            value={user_password}
+                            onChange={(e: any) => setPassword(e.target.value)}
+                        />
+                        <Login onClick={() => store.login(user_email, user_password)}>Войти</Login>
+                    </LoginBlock>
+                </LoginContainer>
             </MainComponents>
         </>
     )
 }
 export default LoginPage;
 
+const LoginContainer = tw.div`py-[50px]`
+const Title = tw.h1`text-center`
 const LoginBlock = tw.div`w-[30%] block mx-auto`;
 const Login = tw.button`block w-[80%] bg-[#f23132] mx-auto mt-[15px] py-[10px] border-none text-white text-[15px] rounded-[5px] cursor-pointer hover:shadow-md duration-200 hover:duration-200 hover:bg-[#df2b2c]`;
